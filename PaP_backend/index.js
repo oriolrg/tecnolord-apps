3a23e112789033604d44e5089c22609da79687ac
Oriol Riu <oriol.riu@gencat.cat>
2026-02-03 13:08:52 +0100

    PaP frontend

--- PaP_backend/index.js
+++ PaP_backend/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 
 // 1. Servir fitxers estàtics
-// Com que Caddy ja ha tret la paraula "/opos", aquí usem "/"
+// Com que Caddy ja ha tret la paraula "/pap", aquí usem "/"
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // 2. API Ping
-// Com que Caddy ha tret "/api/opos", aquí només posem "/ping"
+// Com que Caddy ha tret "/api/pap", aquí només posem "/ping"
 app.get('/ping', (req, res) => {
-    res.json({ ok: true, msg: "Backend d'Opos operatiu" });
+    res.json({ ok: true, msg: "Backend d'PaP operatiu" });
 });
 
 // 3. Fallback per a Single Page Application
@@ -17,6 +17,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(5000, '0.0.0.0', () => {
-    console.log("Opos backend a punt al port 5000");
+app.listen(6000, '0.0.0.0', () => {
+    console.log("PaP backend a punt al port 6000");
 });
