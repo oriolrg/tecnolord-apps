fc20d7b57c99cfaa8f783d1d1d6a3dc72773b81f
Oriol Riu <oriol.riu@gencat.cat>
2026-02-04 10:11:56 +0100

    brujola

--- orientatrack/backend/index.js
+++ orientatrack/backend/index.js
@@ -26,3 +26,9 @@ app.get('*', (req, res) => {
 app.listen(port, '0.0.0.0', () => {
   console.log(`OrientaTrack funcionant a http://localhost:${port}`);
 });
+// Això servirà els fitxers de 'public' quan la URL comenci per /orientatrack
+// Exemple: /orientatrack/js/compass.js -> buscarà a public/js/compass.js
+app.use('/orientatrack', express.static(path.join(__dirname, 'public')));
+
+// També servim l'arrel per si de cas o per local
+app.use(express.static(path.join(__dirname, 'public')));
